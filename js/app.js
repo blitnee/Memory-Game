7ce16a997201e374f0551562a33658c10bf96b0c
blitnee <brittneelc@gmail.com>
2018-02-27 12:03:53 -0500

    Remove .show

--- js/app.js
+++ js/app.js
@@ -118,18 +118,42 @@
         (function resetTime() {
             game.seconds = 0;
             game.minutes = 0;
-            game.hours = 0;
+            game.playerTime = 0;
         })();
+
+        function setTimer() {
+            game.ui.htmlDeck.removeEventListener('click', setTimer);
+
+            function add() {
+                game.seconds++;
+                if (game.seconds >= 60) {
+                    game.seconds = 0;
+                    game.minutes++;
+                    if (game.minutes >= 60) {
+                        game.minutes = 0;
+                    }
+                }
+                game.ui.time.textContent = (game.minutes ? (game.minutes > 9 ? game.minutes : "0" + game.minutes) : "00") + ":" + (game.seconds > 9 ? game.seconds : "0" + game.seconds);
+                ((game.minutes === 00 && game.seconds === 31) || (game.minutes === 00 && game.seconds === 46) || (game.minutes === 01 && game.seconds === 01)) ? game.ui.stars.removeChild(game.ui.stars.lastElementChild) : false;
+                (game.minutes === 02) ? winLose() : false;
+                timer();
+                return game.playerTime = game.ui.time.textContent;
+            }
+
+            function timer() {
+                game.t = setTimeout(add, 1000);
+            }
+            timer();
+        };
 
     }
 
-    // Clear cards with game play classes
     function clear(array) {
         setTimeout(function() {
             array.map(function(elem) {
-                elem.parentNode.classList.remove('show', 'open', 'match');
+                elem.parentNode.classList.remove('open', 'match');
             });
-        }, 500);
+        }, 300);
     }
 
     function match(array) {
